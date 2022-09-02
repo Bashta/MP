77a2c58c192f1a49a26edfec8d382930dd3bd8c3
Erison Veshi <Erisonve@gmail.com>
2022-09-02 16:24:47 +0200

    Initial Commit

---
+++ Shared/MP_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MP_APPApp.swift
+//  Shared
+//
+//  Created by Erison Veshi on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MP_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
